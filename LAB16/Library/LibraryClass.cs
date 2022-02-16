39913374dd6313c7d2141449201a29985e3608da
Настя Рис <nastyarisunkova@gmail.com>
2022-02-16 14:36:46 +0300

    Билиотека готова

---
+++ LAB16/Library/LibraryClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Library
+{
+    public class LibraryClass
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
