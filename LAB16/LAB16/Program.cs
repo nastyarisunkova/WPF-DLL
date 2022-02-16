2f05d2d6bce340111f6d3bb5cf3380b5578aed43
Настя Рис <nastyarisunkova@gmail.com>
2022-02-16 15:02:06 +0300

    Библиотека подключена

--- LAB16/LAB16/Program.cs
+++ LAB16/LAB16/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace LAB16
 {
@@ -10,6 +7,17 @@ namespace LAB16
     {
         static void Main(string[] args)
         {
+            Assembly a = Assembly.Load("Library");
+            Object o = a.CreateInstance("LibraryClass");
+            Type t = a.GetType("LibraryClass");
+
+            Object[] numbers = { 5, 3 };
+
+            MethodInfo mi = t.GetMethod("Add");
+            Console.WriteLine(mi.Invoke(o, numbers));
+
+            mi = t.GetMethod("Sub");
+            Console.WriteLine(mi.Invoke(o,numbers));
         }
     }
 }
--- LAB16/Library/LibraryClass.cs
+++ LAB16/Library/LibraryClass.cs
@@ -1,16 +1,11 @@
-﻿using System;
-
-namespace Library
+﻿public class LibraryClass
 {
-    public class LibraryClass
+    public int Add(int a, int b)
     {
-        public int Add(int a, int b)
-        {
-            return a + b;
-        }
-        public int Sub(int a, int b)
-        {
-            return a - b;
-        }
+        return a + b;
+    }
+    public int Sub(int a, int b)
+    {
+        return a - b;
     }
 }
